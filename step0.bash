49a335b46e51532fea08fe1b5dd7b3d8afb4030c
Tiryoh <tiryoh@gmail.com>
2016-05-11 19:59:57 +0900

    Add a script for setting time to step0.bash

--- step0.bash
+++ step0.bash
@@ -2,11 +2,16 @@
 
 # This script is required only when you did not get this script via Git.
 
-### setup git and openssh ###
+### setup curl,git and openssh ###
 
 sudo apt-get update
+sudo apt-get install -y curl
 sudo apt-get install -y git
 sudo apt-get install -y openssh-server
+
+### setup date
+
+sudo date -s "$(curl -s --head http://www.google.co.jp | grep '^Date' | cut -b 7-)"
 
 ### cloning this repository ###
 
