8477fec25d5b59a2e9813a9e26609f6dcbfebcc5
Tiryoh <tiryoh@gmail.com>
2016-05-12 17:05:08 +0900

    Fix issue #3, travis-ci error
    Add python ros packeges

--- step1.bash
+++ step1.bash
@@ -1,5 +1,7 @@
 #!/bin/bash -exv
 
+export ROS_DISTRO=indigo
+
 echo 'deb http://packages.ros.org/ros/ubuntu trusty main' > /tmp/$$-deb
 sudo mv /tmp/$$-deb /etc/apt/sources.list.d/ros-latest.list
 
@@ -9,7 +11,7 @@ sudo apt-get update
 
 sudo apt-get install -y ros-indigo-ros-base
 sudo apt-get install -y python-rosinstall
-sudo apt-get install -y python-rosdep
+sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-catkin
 sudo apt-get install -y build-essential
 sudo apt-get install -y linux-headers-$(uname -r)
 
